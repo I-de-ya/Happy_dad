cbe40e7334e2d40ce59eca3d66faf0cbd8db4474
inner-whisper <maks.rydkin@gmail.com>
2011-11-08 19:30:32 +0400

    Первый коммит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyDad::Application.config.secret_token = '6b1e61abd5c93cc8ad83b74449e944c18cff3e32662c5d39a47209f528f2eaa41386c7401457251bbb6cc40adf6b1dfb1eb553a4abcc2791671be2c2e3c50770'
