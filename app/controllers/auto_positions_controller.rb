57c6bde1d7ffea99881b46b7f0c169cc77d23f8a
inner-whisper <maks.rydkin@gmail.com>
2011-11-19 12:55:33 +0400

    Some experiments with import csv

--- app/controllers/auto_positions_controller.rb
+++ app/controllers/auto_positions_controller.rb
@@ -1,9 +1,20 @@
+# coding: utf-8
 class AutoPositionsController < ApplicationController
 
   def index
   	@auto_positions = AutoPosition.all
   end
 
+=begin
+  def import_from_csv
+  	require 'csv'
+  	CSV.open('/home/innerwhisper/Рабочий стол/Import1.csv', 'r').each do |row|
+  		AutoPosition.create(:automation_area => row[0], :technological_unit => row[1], :project_specification => row[2], :title => row[3])
+  	end
+  	redirect_to auto_positions_path
+  end
+=end
+  
   def new
   end
 
