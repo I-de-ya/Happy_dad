4935dd900cd761e6a9fe7d849cad70dc2b060c8f
Max <maks.rydkin@gmail.com>
2012-01-04 17:03:34 +0400

    Complete replacement import

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -14,7 +14,7 @@ class DevicesController < ApplicationController
 	
 		respond_to do |format|
 			format.html
-			format.xls { send_data @devices.to_xls(:columns => [:title, {:replacement => [:title]}], :headers => ["С позиции", "На позицию"]), :filename => 'Замены приборов.xls'}
+			format.xls { send_data @devices.to_xls(:columns => [{:auto_position => [:automation_area,:technological_unit,:title]},:input_range,:input_measurement_units,:output_range,:output_measurement_units,:device_type,:model,:serial_number, {:replacement => [:input_range,:input_measurement_units,:output_range,:output_measurement_units,:device_type,:model,:serial_number,:form_of_mr,:prev_mr_date,:next_mr_date]}], :headers => ["С позиции", "На позицию"]), :filename => 'Замены приборов.xls'}
 		end
 	end
 	
